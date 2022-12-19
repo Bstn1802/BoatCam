0ac4245f74842d1a9bd0eb71ed3785134f3b511e
fc1xeon <96582306+fc1xeon@users.noreply.github.com>
2022-12-19 21:25:05 +0800

    Update BoatCamConfig.java

--- src/main/java/boatcam/config/BoatCamConfig.java
+++ src/main/java/boatcam/config/BoatCamConfig.java
@@ -16,7 +16,7 @@ public final class BoatCamConfig implements ConfigData {
     @Comment("Sensitivity of mouse steering, on a semitone scale.")
     @BoundedDiscrete(min = -24, max = 24)
     private int sensitivity = 0;
-    @Comment("How quickly the camera should follow the calculated angle.\nFixed Mode - No effect\nKartrider Mode - corresponds to critical damping factor\nClassic Mode - corresponds to average-weighting.")
+    @Comment("How quickly the camera should follow the calculated angle.\nFixed - No effect\nKartrider - adjusts critical damping factor\nClassic - adjusts smoothing-weights.")
     @BoundedDiscrete(min = 1, max = 100)
     private int smoothness = 50;
     @Comment("Perspective when riding a boat in boat mode. Perspective wont change when this is set to none.")
